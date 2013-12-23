126135ce0c9895ce793718bb672223712f2ca3ba
MK Office 01 <costa@halicea.com>
2013-12-23 19:26:50 +0100

    Added select element to choose between applications. Started coding for routing ( not finished ). Added the bootstrap template.

--- automator_project/src/hal_configurator/web/static/app.js
+++ automator_project/src/hal_configurator/web/static/app.js
@@ -1,20 +1,32 @@
  // var mockDataForThisTest = "json=" + encodeURI(JSON.stringify('bc.json'));
-
+'use strict';
 
 var app = angular.module('myApp', []);
 app.config(function($httpProvider){
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
-
-app.controller('JsonCtrl', function($scope, $http, $log){
+// Application Identifier doesn't need to show
+app.controller('ConfigCtrl', function($scope, $http, $log){
     $scope.config = {};
-    $http.get('http://staging.mediawiremobile.com:5001/config/FloridaDesign/IOS/bc.json'
-    ).success(function(data, status) {
+    $scope.configuraitons = [];
+    $scope.configId = 'FloridaDesign';
+    $scope.platform = 'IOS';
+    $http.get('/config').success(function(data){
+        $scope.configurations = data;
+    }).error(function(status, headers, response, config){
+        $log.warn(status, headers, response, config);
+    });
+    $scope.reloadConfig = function(configId){
+        $http.get('/config/'+$scope.configId+'/'+$scope.platform+'/bc.json'
+            ).success(function(data, status) {
             $scope.config = data;
-    }).error(function(a, b, c, d){
-		 $log.log(a);
-		 $log.log(b);
-		 $log.log(c);
-		 $log.log(d);
-	});
+        }).error(function(status, headers, response, config){
+            $log.warn(status, headers, response, config);
+        });
+    };
 });
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/partial1', {templateUrl: 'partial1.html', controller: 'ViewCtrl'});
+        $routeProvider.when('/partial2', {templateUrl: 'partial2.html', controller: 'ViewCtrl'});
+        $routeProvider.otherwise({redirectTo: '/partial1'});
+    }]);
--- automator_project/src/hal_configurator/web/web_server.py
+++ automator_project/src/hal_configurator/web/web_server.py
@@ -1,16 +1,19 @@
-from flask import Flask, request, Response # @UnusedImport
+from flask import Flask, request, Response,send_from_directory # @UnusedImport
 import os
 import json
+import sys
+sys.path.append('../../');
 from hal_configurator.lib.config_loaders import FileConfigLoader
 from attributes import crossdomain
-#setup the working directory to thisone
-#
-workspace_path = '/home/costa.halicea/MediawireConfigurations'
-app = Flask(__name__, static_folder='./static', static_url_path='')
 
-def run_server():
-  os.chdir(os.path.dirname(__file__))
-  app.run(host="0.0.0.0", port=5001,debug=True)
+workspace_path = '/Users/mkoffice01/MyProjects/MediawireConfigurations'
+app = Flask(__name__, static_folder='./static', static_url_path='')
+#os.chdir(os.path.dirname(__file__))
+
+@app.route("/config")
+def list_configs():
+  res =  [x for x in os.listdir(workspace_path) if os.path.isdir(os.path.join(workspace_path, x)) and not x.startswith('.')]
+  return Response(response=json.dumps(res), status=200, mimetype="application/json")
 
 @app.route("/config/<identifier>/<platform>/<name>")
 @crossdomain(origin="*")
@@ -35,5 +38,9 @@ def save_json(identifier,platform, name):
   except Exception, ex:  # @UnusedVariable
     return False
 
-if __name__ == '__main__':
-  run_server()
+@app.route('/config/<path:filename>')
+def base_static(filename):
+    return send_from_directory(workspace_path, filename)
+
+app.run(host="0.0.0.0", port=5001,debug=True)
+
