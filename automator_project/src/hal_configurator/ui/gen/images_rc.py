b6973acdba849a596a2a9d6ef8b449e3ea67e065
Costa Halicea <costa@halicea.com>
2013-12-23 04:03:48 +0100

    no message

--- automator_project/src/hal_configurator/ui/gen/images_rc.py
+++ automator_project/src/hal_configurator/ui/gen/images_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Wed Dec 4 11:56:02 2013
+# Created: Sun Dec 22 01:10:29 2013
 #      by: The Resource Compiler for PySide (Qt v4.8.1)
 #
 # WARNING! All changes made in this file will be lost!
