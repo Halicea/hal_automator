4a90a29b49179f0148cf5e78fc405c3e55094d86
Robert Markovski <robertmarkovski@yahoo.com>
2013-12-26 21:46:43 +0100

    Cleanup by Costa. Added new service.

--- automator_project/src/hal_configurator/web/static/app.js
+++ automator_project/src/hal_configurator/web/static/app.js
@@ -4,9 +4,3 @@ var app = angular.module('myApp', []);
 app.config(function($httpProvider){
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
-
-var phonecatApp = angular.module('phonecatApp', [
-    'ngRoute',
-    'phonecatControllers'
-]);
-
---
+++ automator_project/src/hal_configurator/web/static/services/configSvc.js
@@ -0,0 +1,34 @@
+'use strict';
+app.factory('configSvc', function($http, $log){
+	var result = {};
+    result.index = function(callback){
+        $http.get('/config').success(function(data){
+           callback(data);
+        }).error(function(status, headers, response, config){
+            $log.warn(status, headers, response, config);
+            callback([]);
+        });
+    };
+    
+    result.get = function(id, platform, callback){
+        $http.get('/config/'+id+'/'+platform+'/bc.json').success(function(data, status) {
+            callback(data);
+        }).error(function(status, headers, response, config){
+            callback(null);
+            $log.warn(status, headers, response, config);
+        });
+    };
+
+    result.save = function(id, platform, config, callback){
+       $http.post('/config/'+id+'/'+platform+'/bc.json', config)
+        .success(function(data, status) {
+            callback(data);
+        }).error(function(status, headers, response, config){
+            callback({
+                errors:['Server threw an error']
+            });
+            $log.warn(status, headers, response, config);
+        });
+    };
+    return result;
+});
