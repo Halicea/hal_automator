412336725c959ffeb19d19120120a3e3da94b97a
Robert Markovski <robertmarkovski@yahoo.com>
2013-12-29 02:02:44 +0100

    Committing the changes I have made for routing. There is some error with the configSvc importing it crashes my browser if I import that script.

--- automator_project/src/hal_configurator/web/static/js/app.js
+++ automator_project/src/hal_configurator/web/static/js/app.js
@@ -1,6 +1,71 @@
 'use strict';
 
-var app = angular.module('myApp', []);
-app.config(function($httpProvider){
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(function($httpProvider, $routeProvider){
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    $routeProvider.
+        when('/HomePage', {
+            templateUrl: 'homepage.html',
+            controller: 'HomePageCtrl'
+        }).
+        when('/NewConfig', {
+            templateUrl: 'newconfig.html',
+            controller: 'ConfigCtrl'
+        }).
+        when('/EditConfig', {
+            templateUrl: 'editconfig.html',
+            controller: 'EditConfigCtrl'
+        }).
+        when('/Certificates', {
+            templateUrl: 'certificates.html',
+            controller: 'CertificatesCtrl'
+        });
+});
+
+
+app.controller('EditConfigCtrl', function($scope) {
+    $scope.message = "EditConfigCtrl";
+});
+
+
+app.controller('HomePageCtrl', function($scope) {
+    $scope.message = "HomePageCtrl";
+});
+
+app.controller('CertificatesCtrl', function($scope) {
+    $scope.message = "CertificatesCtrl";
+});
+
+app.controller('ConfigCtrl', function($scope, $log, configSvc){
+    $scope.config = {};
+    $scope.configuraitons = [];
+    $scope.configId = 'FloridaDesign';
+    $scope.platform = 'IOS';
+    $scope.platforms = ['IOS', 'Android'];
+    var refreshConfig = function(){
+        configSvc.get($scope.configId, $scope.platform, function(conf){
+            $scope.config = conf;
+        });
+    };
+    //refreshConfig();
+    configSvc.index(function(result){
+        $scope.configurations =  _.union(['-- Select Item --'], result);
+    });
+
+    $scope.reloadConfig = function(configId){
+        $scope.configId = configId;
+        refreshConfig();
+    };
+    $scope.reloadPlatform = function(platform){
+        $scope.platform = platform;
+        refreshConfig();
+    };
+    $scope.saveConfig = function() {
+        configSvc.save($scope.configId, $scope.platform, $scope.config, function(validationResult){
+            if(validationResult && validationResult.errors){
+                alert(validationResult.errors);
+            }
+        });
+    };
 });
