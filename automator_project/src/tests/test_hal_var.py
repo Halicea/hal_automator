55cd9d7c23e9ebfeef01ca4bcbdfd6b242db7d3f
Costa Halicea <costa@halicea.com>
2014-04-06 22:38:41 +0200

    tests directory added and implemented tests for the variables and resources

---
+++ automator_project/src/tests/test_hal_var.py
@@ -0,0 +1,64 @@
+'''
+Created on Apr 5, 2014
+
+@author: Costa Halicea
+'''
+import unittest
+from collections import namedtuple
+from hal_configurator.lib.models.hal_var import HalVar
+
+
+class TestHalVar(unittest.TestCase):
+
+    def setUp(self):
+        self.var_values = {
+          'value':'test_value',
+          'name':'sample_var',
+          'display':'Sample Variable',
+          'helptext':'Help Text',
+          'validators':[1, 2],
+          'admin_only':True,
+          'required':False,
+          'editable':False
+        }
+    def tearDown(self):
+        self.var_values = None
+        self.test_var = None
+
+    def testHalVarCreation(self):
+        self.test_var = HalVar.from_dict(self.var_values)
+        for k in self.var_values.keys():
+          msg = 'variable %s has value %s and it was set as %s '%(k, self.test_var[k], self.var_values[k])
+          self.assertEquals(self.var_values[k], self.test_var[k], msg)
+
+    def testIterationOfTheKeys(self):
+      self.test_var = HalVar.from_dict(self.var_values)
+      for k in self.test_var.keys():
+        msg = 'variable %s has value %s and it was set as %s '%(k, self.test_var[k], self.var_values[k])
+        self.assertEquals(self.var_values[k], self.test_var[k], msg)
+
+    def testIteration(self):
+      self.test_var = HalVar.from_dict(self.var_values)
+      d = dict( [(k, v) for k, v in self.test_var.items()])
+      self.assertEquals(len(d.keys()), len(self.var_values.keys()), 'Keys dont match')
+      for k in d.keys():
+        self.assertTrue(self.var_values.has_key(k), 'Unknown key %s'%k)
+
+    def testToDictionary(self):
+      self.test_var = HalVar.from_dict(self.var_values)
+      self.assertEquals(len(self.var_values.keys()), len(self.test_var.dictionary.keys()))
+
+    def testDictionarySet(self):
+      self.test_var = HalVar()
+      for k in self.var_values.keys():
+          msg = 'variable %s has value %s and it was set as %s '%(k, self.test_var[k], self.var_values[k])
+          self.assertNotEquals(self.var_values[k], self.test_var[k], msg)
+      self.test_var.dictionary = self.var_values
+      for k in self.var_values.keys():
+          msg = 'variable %s has value %s and it was set as %s '%(k, self.test_var[k], self.var_values[k])
+          self.assertEquals(self.var_values[k], self.test_var[k], msg)
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'TestHalVar.testHalVarCreation', 'TestHalVar.testIterationOfTheKeys']
+    unittest.main()
+
