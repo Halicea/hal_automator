c09914eec0582b889eb3f319105c964f15a6c9db
Sam Hansen <hansens89@gmail.com>
2014-12-03 13:02:20 -0500

    completed most of the layout structure

--- automator_project/src/hal_configurator/web/static/js/scripts.js
+++ automator_project/src/hal_configurator/web/static/js/scripts.js
@@ -3,3 +3,153 @@ $(document).ready(function() {
     $('.row-offcanvas').toggleClass('active');
   });
 });
+
+function readURL(input) {
+if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#multi-up1')
+            .attr('src', e.target.result)
+            .width(150)
+            .height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+}
+}
+
+function readURL2(input) {
+if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#multi-up2')
+            .attr('src', e.target.result)
+            .width(150)
+            .height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+}
+}
+
+function readURL3(input) {
+if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#multi-up3')
+            .attr('src', e.target.result)
+            .width(150)
+            .height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+}
+}
+
+function readURL4(input) {
+if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#multi-up4')
+            .attr('src', e.target.result)
+            .width(150)
+            .height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+}
+}
+
+function readURL5(input) {
+if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#multi-up5')
+            .attr('src', e.target.result)
+            .width(150)
+            .height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+}
+}
+
+function readURL6(input) {
+if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#multi-up6')
+            .attr('src', e.target.result)
+            .width(150)
+            .height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+}
+}
+
+function readURL7(input) {
+if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#multi-up7')
+            .attr('src', e.target.result)
+            .width(150)
+            .height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+}
+}
+
+function readURL8(input) {
+if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#multi-up8')
+            .attr('src', e.target.result)
+            .width(150)
+            .height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+}
+}
+
+function readURL9(input) {
+if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#multi-up9')
+            .attr('src', e.target.result)
+            .width(150)
+            .height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+}
+}
+
+function readURL10(input) {
+if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#multi-up10')
+            .attr('src', e.target.result)
+            .width(150)
+            .height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+}
+}
