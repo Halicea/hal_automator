69a847ede547f528ef6cc8a9f4e6a605af40c420
Sam Hansen <hansens89@gmail.com>
2014-12-04 18:05:27 -0500

    Completed most structure and functionality

--- automator_project/src/hal_configurator/web/static/js/scripts.js
+++ automator_project/src/hal_configurator/web/static/js/scripts.js
@@ -1,3 +1,181 @@
+
+
+$( ".col-box1" ).click(function() {
+  $( ".col-box1" ).empty();
+});
+
+$( ".col-box2" ).click(function() {
+  $( ".col-box2" ).empty();
+});
+
+$( ".col-box3" ).click(function() {
+  $( ".col-box3" ).empty();
+});
+
+$( ".col-box4" ).click(function() {
+  $( ".col-box4" ).empty();
+});
+
+$( ".col-box5" ).click(function() {
+  $( ".col-box5" ).empty();
+});
+
+$( ".col-box6" ).click(function() {
+  $( ".col-box6" ).empty();
+});
+
+$(document).ready(function() {
+   $('.color-box').colpick({
+    colorScheme:'light',
+    layout:'rgbhex',
+    color:'ff8800',
+    onSubmit:function(hsb,hex,rgb,el) {
+        $(el).css('background-color', '#'+hex);
+        $(el).colpickHide();
+    }
+})
+});
+
+$(document).ready(function() {
+    if($('#general-ios').hasClass('active')) {
+        $( '#general-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#general-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+}
+if($('#general-android').hasClass('active')) {
+        $( '#general-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#general-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+}
+$('#general-android').click(function() {
+        $( '#general-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#general-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+})
+$('#general-ios').click(function() {
+        $( '#general-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#general-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+})
+
+});
+
+$(document).ready(function() {
+    if($('#publisher-info-ios').hasClass('active')) {
+        $( '#publisher-info-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#publisher-info-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+}
+if($('#publisher-info-android').hasClass('active')) {
+        $( '#publisher-info-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#publisher-info-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+}
+$('#publisher-info-android').click(function() {
+        $( '#publisher-info-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#publisher-info-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+})
+$('#publisher-info-ios').click(function() {
+        $( '#publisher-info-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#publisher-info-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+})
+
+});
+
+$(document).ready(function() {
+    if($('#design-ios').hasClass('active')) {
+        $( '#design-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#design-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+}
+if($('#design-android').hasClass('active')) {
+        $( '#design-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#design-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+}
+$('#design-android').click(function() {
+        $( '#design-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#design-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+})
+$('#design-ios').click(function() {
+        $( '#design-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#design-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+})
+
+});
+
+$(document).ready(function() {
+    if($('#platform-specifics-ios').hasClass('active')) {
+        $( '#platform-specifics-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#platform-specifics-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+}
+if($('#platform-specifics-android').hasClass('active')) {
+        $( '#platform-specifics-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#platform-specifics-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+}
+$('#platform-specifics-android').click(function() {
+        $( '#platform-specifics-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#platform-specifics-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+})
+$('#platform-specifics-ios').click(function() {
+        $( '#platform-specifics-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#platform-specifics-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+})
+
+});
+
+$(document).ready(function() {
+    if($('#build-app-ios').hasClass('active')) {
+        $( '#build-app-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#build-app-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+}
+if($('#build-app-android').hasClass('active')) {
+        $( '#build-app-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#build-app-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+}
+$('#build-app-android').click(function() {
+        $( '#build-app-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#build-app-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+})
+$('#build-app-ios').click(function() {
+        $( '#build-app-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#build-app-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+})
+
+});
+
+$(document).ready(function() {
+    if($('#store-general-ios').hasClass('active')) {
+        $( '#store-general-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#store-general-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+}
+if($('#store-general-android').hasClass('active')) {
+        $( '#store-general-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#store-general-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+}
+$('#store-general-android').click(function() {
+        $( '#store-general-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#store-general-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+})
+$('#store-general-ios').click(function() {
+        $( '#store-general-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#store-general-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+})
+
+});
+
+$(document).ready(function() {
+    if($('#store-artwork-ios').hasClass('active')) {
+        $( '#store-artwork-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#store-artwork-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+}
+if($('#store-artwork-android').hasClass('active')) {
+        $( '#store-artwork-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#store-artwork-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+}
+$('#store-artwork-android').click(function() {
+        $( '#store-artwork-ios-icon' ).css( "background", "url('../img/ico_ios_inactive.png')" );
+        $( '#store-artwork-android-icon' ).css( "background", "url('../img/ico_android_active.png')" );
+})
+$('#store-artwork-ios').click(function() {
+        $( '#store-artwork-ios-icon' ).css( "background", "url('../img/ico_ios_active.png')" );
+        $( '#store-artwork-android-icon' ).css( "background", "url('../img/ico_android_inactive.png')" );
+})
+
+});
+
 $(document).ready(function() {
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
@@ -94,62 +272,3 @@ if (input.files && input.files[0]) {
 }
 }
 
-function readURL7(input) {
-if (input.files && input.files[0]) {
-    var reader = new FileReader();
-
-    reader.onload = function (e) {
-        $('#multi-up7')
-            .attr('src', e.target.result)
-            .width(150)
-            .height(200);
-    };
-
-    reader.readAsDataURL(input.files[0]);
-}
-}
-
-function readURL8(input) {
-if (input.files && input.files[0]) {
-    var reader = new FileReader();
-
-    reader.onload = function (e) {
-        $('#multi-up8')
-            .attr('src', e.target.result)
-            .width(150)
-            .height(200);
-    };
-
-    reader.readAsDataURL(input.files[0]);
-}
-}
-
-function readURL9(input) {
-if (input.files && input.files[0]) {
-    var reader = new FileReader();
-
-    reader.onload = function (e) {
-        $('#multi-up9')
-            .attr('src', e.target.result)
-            .width(150)
-            .height(200);
-    };
-
-    reader.readAsDataURL(input.files[0]);
-}
-}
-
-function readURL10(input) {
-if (input.files && input.files[0]) {
-    var reader = new FileReader();
-
-    reader.onload = function (e) {
-        $('#multi-up10')
-            .attr('src', e.target.result)
-            .width(150)
-            .height(200);
-    };
-
-    reader.readAsDataURL(input.files[0]);
-}
-}
