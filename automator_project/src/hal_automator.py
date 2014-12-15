b7709a7753ba5781a02f7d18f7f7e29b850e2ee7
Halicea <costa@halicea.com>
2014-12-15 10:54:47 +0100

    cleaned up the bower componnents and refactored the app.py in the web package

--- automator_project/src/hal_automator.py
+++ automator_project/src/hal_automator.py
@@ -3,52 +3,52 @@ import sys
 import os
 from hal_configurator.lib.app_config import config
 from hal_configurator.lib.plugin_loader import load_plugins_from_directory_list
-
 if config:
-  load_plugins_from_directory_list(config.plugin_dirs)
+    load_plugins_from_directory_list(config.plugin_dirs)
 else:
-  print 'No Configuration Loadeed, Continuing...'
+    print 'No Configuration Loadeed, Continuing...'
 
 sys.path.append(os.path.abspath(__file__))
 try:
-  import hal_configurator.lib.configurator_console as console
+    import hal_configurator.lib.configurator_console as console
 except:
-  print 'Not configured for executor, only web server mode is possible'
+    print 'Not configured for executor, only web server mode is possible'
 
-print "Running in",os.path.abspath(__file__)
-ui_enabled= True
+print "Running in", os.path.abspath(__file__)
+ui_enabled = True
 try:
-  from PySide import QtGui
+    from PySide import QtGui
 except Exception, ex:
-  ui_enabled = False
-  print ex
-  print "No Qt Installed"
+    ui_enabled = False
+    print ex
+    print "No Qt Installed"
 if ui_enabled:
-  from hal_configurator.ui.configwindow import ConfigWindow
+    from hal_configurator.ui.configwindow import ConfigWindow
+
 
 def main(isAdmin):
-  """
-  Main Application Runner
-  :param: isAdmin
-  :type : isAdmin: bool
-  """
-  app = QtGui.QApplication(sys.argv)
-  app.setStyle('windows')
-  mw = ConfigWindow(None)
-  mw.show()
-  app.setWindowIcon(QtGui.QIcon())
-  sys.exit(app.exec_())
+    """
+    Main Application Runner
+    :param: isAdmin
+    :type : isAdmin: bool
+    """
+    app = QtGui.QApplication(sys.argv)
+    app.setStyle('windows')
+    mw = ConfigWindow(None)
+    mw.show()
+    app.setWindowIcon(QtGui.QIcon())
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
-  if len(sys.argv)==1 :
-    main(False)
-  elif sys.argv[1] =='-admin':
-    main(True)
-  elif sys.argv[1]=='-web':
-    try:
-      import hal_configurator.web.web_server as web_server
-    except:
-      print 'Not Configured for web mode'
-    web_server.run_server()
-  else:
-    console.main()
+    if len(sys.argv) == 1:
+        main(False)
+    elif sys.argv[1] == '-admin':
+        main(True)
+    elif sys.argv[1] == '-web':
+        try:
+            import hal_configurator.web.web_server as web_server
+        except:
+            print 'Not Configured for web mode'
+        web_server.run_server()
+    else:
+        console.main()
--- automator_project/src/hal_configurator/web/app.py
+++ automator_project/src/hal_configurator/web/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, Response,send_from_directory, jsonify, send_file,abort # @UnusedImport
+from flask import (
+    Flask, request, Response, send_from_directory, jsonify,
+    send_file, abort)  # @UnusedImport
 import os
 import json
 import sys
@@ -11,31 +13,35 @@ sys.path.append(root_dir)
 from hal_configurator.lib.config_loaders import FileConfigLoader
 from attributes import crossdomain
 
-app_config = json.loads(open(os.path.join(current_dir,'config.json'), 'r').read())
+app_config = json.loads(open(os.path.join(current_dir, 'config.json'), 'r').read())
 workspace_path = os.path.expanduser(app_config['workspace_path'])
 app = Flask(__name__, static_folder='./static', static_url_path='')
-#os.chdir(os.path.dirname(__file__))
+# os.chdir(os.path.dirname(__file__))
+
 @app.route('/')
 def root():
   return app.send_static_file('index.html')
 
 @app.route("/config")
 def list_configs():
-  res =  [x for x in os.listdir(workspace_path) if os.path.isdir(os.path.join(workspace_path, x)) and not x.startswith('.')]
+  res = [x for x in os.listdir(workspace_path)
+         if os.path.isdir(os.path.join(workspace_path, x)) and
+         not (x.startswith('.') or x.startswith('_'))]
   return Response(response=json.dumps(res), status=200, mimetype="application/json")
 
 @app.route("/config/<identifier>/<platform>/<name>")
 @crossdomain(origin="*")
-def get_config(identifier,platform, name):
-  filepath = os.path.join(workspace_path, identifier,platform,name)
+def get_config(identifier, platform, name):
+  filepath = os.path.join(workspace_path, identifier, platform, name)
   conf = FileConfigLoader(filepath).dictionary
   return Response(response=json.dumps(conf), status=200, mimetype="application/json")
 
+
 @app.route("/config/<identifier>/<platform>/<name>", methods=["POST"])
 @crossdomain(origin="*")
-def save_config(identifier,platform, name):
+def save_config(identifier, platform, name):
   print "saved"
-  filepath = os.path.join(workspace_path, identifier,platform,name)
+  filepath = os.path.join(workspace_path, identifier, platform, name)
   config_loader = FileConfigLoader(filepath)
   try:
     newconf = json.loads(request.data)
@@ -49,29 +55,29 @@ def save_config(identifier,platform, name):
 
 @app.route("/config/<identifier>/<platform>/<name>/var/<varname>", methods=["GET"])
 @crossdomain(origin="*")
-def get_variable(identifier,platform, name, varname):
-  filepath = os.path.join(workspace_path, identifier,platform,name)
+def get_variable(identifier, platform, name, varname):
+  filepath = os.path.join(workspace_path, identifier, platform, name)
   conf = FileConfigLoader(filepath).dictionary
-  res= [x for x in conf["Variables"] if x["name"] == varname][0]
+  res = [x for x in conf["Variables"] if x["name"] == varname][0]
   return Response(response=json.dumps(res), status=200, mimetype="application/json")
 
 @app.route("/config/<identifier>/<platform>/<name>/<varname>", methods=["POST"])
 @crossdomain(origin="*")
-def save_variable(identifier,platform, name, varname):
-  filepath = os.path.join(workspace_path, identifier,platform,name)
+def save_variable(identifier, platform, name, varname):
+  filepath = os.path.join(workspace_path, identifier, platform, name)
   config_loader = FileConfigLoader(filepath)
   conf = config_loader.dictionary
-  res= [x for x in conf["Variables"] if x["name"] == varname][0]
+  res = [x for x in conf["Variables"] if x["name"] == varname][0]
   res["value"] = request.data
   config_loader.save_config(conf)
   return True
 
 @app.route("/config/<identifier>/<platform>/<name>/res/<resid>", methods=["GET"])
 @crossdomain(origin="*")
-def get_resource(identifier,platform, name, resid):
-  filepath = os.path.join(workspace_path, identifier,platform,name)
+def get_resource(identifier, platform, name, resid):
+  filepath = os.path.join(workspace_path, identifier, platform, name)
   conf = FileConfigLoader(filepath).dictionary
-  res=[x for x in conf["Resources"] if x["rid"] == resid][0]
+  res = [x for x in conf["Resources"] if x["rid"] == resid][0]
   file_path = os.path.join(workspace_path, identifier, platform, res['url'])
   print file_path
   if os.path.exists(file_path):
@@ -85,33 +91,29 @@ def get_resource(identifier,platform, name, resid):
 @app.route("/config/<identifier>/<platform>/<name>/res/<resid>", methods=["POST"])
 @crossdomain(origin="*")
 def save_resource(identifier, platform, name, resid):
-  filepath = os.path.join(workspace_path, identifier,platform,name)
+  filepath = os.path.join(workspace_path, identifier, platform, name)
   conf = FileConfigLoader(filepath)
-  res=[x for x in conf["Resources"] if x["rid"] == resid][0]
-  data_file = request.files.get('data_file')
-  #filename = data_file.filename
-  print "File Name: %s"%dir(data_file)
-  print data_file
+  res = [x for x in conf["Resources"] if x["rid"] == resid][0]
+  # data_file = request.files.get('data_file')
   files = request.files.getlist('files[]')
-  print "Total Files: %s"%files[0].filename
   file_path = os.path.join(workspace_path, identifier, platform, res['url'])
   files[0].save(file_path)
-  #shutil.copy(data_file, file)
-  print "File Path: %s"% file_path
-  #save_file(data_file, file_name)
-  #file_size = get_file_size(file_name)
-  #file_url = url_for('download', file_name=file_name)
+  print "File Path: %s" % file_path
+  # shutil.copy(data_file, file)
+  # save_file(data_file, file_name)
+  # file_size = get_file_size(file_name)
+  # file_url = url_for('download', file_name=file_name)
   # providing the thumbnail url is optional
-  #thumbnail_url = url_for('thumbnail', file_name=file_name)
+  # thumbnail_url = url_for('thumbnail', file_name=file_name)
   return jsonify(name='test',
                  size=100000,
-                 url='/config/%s/%s/%s'%(identifier, platform, res['url']),
-                 thumbnail='/config/%s/%s/%s'%(identifier, platform, res['url']))
+                 url='/config/%s/%s/%s' % (identifier, platform, res['url']),
+                 thumbnail='/config/%s/%s/%s' % (identifier, platform, res['url']))
 
 
 @app.route('/config/<path:filename>')
 def base_static(filename):
     return send_from_directory(workspace_path, filename)
-if __name__ == '__main__':
-  app.run(host="0.0.0.0", port=5001,debug=True)
 
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=5001, debug=True)
